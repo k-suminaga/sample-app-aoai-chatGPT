b4d761886ee8a320c833ea03c9cf769232e2e2f8
k-suminaga <kazuhiro.suminaga@murata.com>
2023-09-25 19:34:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,8 @@ import openai
 from azure.identity import DefaultAzureCredential
 from flask import Flask, Response, request, jsonify, send_from_directory
 from dotenv import load_dotenv
-
 from backend.auth.auth_utils import get_authenticated_user_details
 from backend.history.cosmosdbservice import CosmosConversationClient
-
 load_dotenv()
 
 app = Flask(__name__, static_folder="static")
@@ -18,16 +16,12 @@ app = Flask(__name__, static_folder="static")
 @app.route("/")
 def index():
     return app.send_static_file("index.html")
-
 @app.route("/favicon.ico")
 def favicon():
     return app.send_static_file('favicon.ico')
-
 @app.route("/assets/<path:path>")
 def assets(path):
     return send_from_directory("static/assets", path)
-
-
 # ACS Integration Settings
 AZURE_SEARCH_SERVICE = os.environ.get("AZURE_SEARCH_SERVICE")
 AZURE_SEARCH_INDEX = os.environ.get("AZURE_SEARCH_INDEX")
@@ -43,8 +37,6 @@ AZURE_SEARCH_URL_COLUMN = os.environ.get("AZURE_SEARCH_URL_COLUMN")
 AZURE_SEARCH_VECTOR_COLUMNS = os.environ.get("AZURE_SEARCH_VECTOR_COLUMNS")
 AZURE_SEARCH_QUERY_TYPE = os.environ.get("AZURE_SEARCH_QUERY_TYPE")
 AZURE_SEARCH_PERMITTED_GROUPS_COLUMN = os.environ.get("AZURE_SEARCH_PERMITTED_GROUPS_COLUMN")
-AZURE_SEARCH_STRICTNESS = os.environ.get("AZURE_SEARCH_STRICTNESS", 3)
-
 # AOAI Integration Settings
 AZURE_OPENAI_RESOURCE = os.environ.get("AZURE_OPENAI_RESOURCE")
 AZURE_OPENAI_MODEL = os.environ.get("AZURE_OPENAI_MODEL")
@@ -60,27 +52,21 @@ AZURE_OPENAI_STREAM = os.environ.get("AZURE_OPENAI_STREAM", "true")
 AZURE_OPENAI_MODEL_NAME = os.environ.get("AZURE_OPENAI_MODEL_NAME", "gpt-35-turbo-16k") # Name of the model, e.g. 'gpt-35-turbo-16k' or 'gpt-4'
 AZURE_OPENAI_EMBEDDING_ENDPOINT = os.environ.get("AZURE_OPENAI_EMBEDDING_ENDPOINT")
 AZURE_OPENAI_EMBEDDING_KEY = os.environ.get("AZURE_OPENAI_EMBEDDING_KEY")
-
-
 SHOULD_STREAM = True if AZURE_OPENAI_STREAM.lower() == "true" else False
-
 # CosmosDB Integration Settings
 AZURE_COSMOSDB_DATABASE = os.environ.get("AZURE_COSMOSDB_DATABASE")
 AZURE_COSMOSDB_ACCOUNT = os.environ.get("AZURE_COSMOSDB_ACCOUNT")
 AZURE_COSMOSDB_CONVERSATIONS_CONTAINER = os.environ.get("AZURE_COSMOSDB_CONVERSATIONS_CONTAINER")
 AZURE_COSMOSDB_ACCOUNT_KEY = os.environ.get("AZURE_COSMOSDB_ACCOUNT_KEY")
-
 # Initialize a CosmosDB client with AAD auth and containers
 cosmos_conversation_client = None
 if AZURE_COSMOSDB_DATABASE and AZURE_COSMOSDB_ACCOUNT and AZURE_COSMOSDB_CONVERSATIONS_CONTAINER:
     try :
         cosmos_endpoint = f'https://{AZURE_COSMOSDB_ACCOUNT}.documents.azure.com:443/'
-
         if not AZURE_COSMOSDB_ACCOUNT_KEY:
             credential = DefaultAzureCredential()
         else:
             credential = AZURE_COSMOSDB_ACCOUNT_KEY
-
         cosmos_conversation_client = CosmosConversationClient(
             cosmosdb_endpoint=cosmos_endpoint, 
             credential=credential, 
@@ -90,22 +76,16 @@ if AZURE_COSMOSDB_DATABASE and AZURE_COSMOSDB_ACCOUNT and AZURE_COSMOSDB_CONVERS
     except Exception as e:
         logging.exception("Exception in CosmosDB initialization", e)
         cosmos_conversation_client = None
-
-
 def is_chat_model():
     if 'gpt-4' in AZURE_OPENAI_MODEL_NAME.lower() or AZURE_OPENAI_MODEL_NAME.lower() in ['gpt-35-turbo-4k', 'gpt-35-turbo-16k']:
         return True
     return False
-
 def should_use_data():
     if AZURE_SEARCH_SERVICE and AZURE_SEARCH_INDEX and AZURE_SEARCH_KEY:
         return True
     return False
-
-
 def format_as_ndjson(obj: dict) -> str:
     return json.dumps(obj, ensure_ascii=False) + "\n"
-
 def fetchUserGroups(userToken, nextLink=None):
     # Recursively fetch group membership
     if nextLink:
@@ -129,37 +109,29 @@ def fetchUserGroups(userToken, nextLink=None):
         return r['value']
     except Exception as e:
         return []
-
-
 def generateFilterString(userToken):
     # Get list of groups user is a member of
     userGroups = fetchUserGroups(userToken)
-
     # Construct filter string
     if userGroups:
         group_ids = ", ".join([obj['id'] for obj in userGroups])
         return f"{AZURE_SEARCH_PERMITTED_GROUPS_COLUMN}/any(g:search.in(g, '{group_ids}'))"
     
     return None
-
-
 def prepare_body_headers_with_data(request):
     request_messages = request.json["messages"]
-
     # Set query type
     query_type = "simple"
     if AZURE_SEARCH_QUERY_TYPE:
         query_type = AZURE_SEARCH_QUERY_TYPE
     elif AZURE_SEARCH_USE_SEMANTIC_SEARCH.lower() == "true" and AZURE_SEARCH_SEMANTIC_SEARCH_CONFIG:
         query_type = "semantic"
-
     # Set filter
     filter = None
     userToken = None
     if AZURE_SEARCH_PERMITTED_GROUPS_COLUMN:
         userToken = request.headers.get('X-MS-TOKEN-AAD-ACCESS-TOKEN', "")
         filter = generateFilterString(userToken)
-
     body = {
         "messages": request_messages,
         "temperature": float(AZURE_OPENAI_TEMPERATURE),
@@ -188,22 +160,17 @@ def prepare_body_headers_with_data(request):
                     "roleInformation": AZURE_OPENAI_SYSTEM_MESSAGE,
                     "embeddingEndpoint": AZURE_OPENAI_EMBEDDING_ENDPOINT,
                     "embeddingKey": AZURE_OPENAI_EMBEDDING_KEY,
-                    "filter": filter,
-                    "strictness": int(AZURE_SEARCH_STRICTNESS)
+                    "filter": filter
                 }
             }
         ]
     }
-
     headers = {
         'Content-Type': 'application/json',
         'api-key': AZURE_OPENAI_KEY,
         "x-ms-useragent": "GitHubSampleWebApp/PublicAPI/2.0.0"
     }
-
     return body, headers
-
-
 def stream_with_data(body, headers, endpoint, history_metadata={}):
     s = requests.Session()
     response = {
@@ -227,7 +194,6 @@ def stream_with_data(body, headers, endpoint, history_metadata={}):
                     response["model"] = lineJson["model"]
                     response["created"] = lineJson["created"]
                     response["object"] = lineJson["object"]
-
                     role = lineJson["choices"][0]["messages"][0]["delta"].get("role")
                     if role == "tool":
                         response["choices"][0]["messages"].append(lineJson["choices"][0]["messages"][0]["delta"])
@@ -240,36 +206,28 @@ def stream_with_data(body, headers, endpoint, history_metadata={}):
                         deltaText = lineJson["choices"][0]["messages"][0]["delta"]["content"]
                         if deltaText != "[DONE]":
                             response["choices"][0]["messages"][1]["content"] += deltaText
-
                     yield format_as_ndjson(response)
     except Exception as e:
         yield format_as_ndjson({"error": str(e)})
-
-
 def conversation_with_data(request_body):
     body, headers = prepare_body_headers_with_data(request)
     base_url = AZURE_OPENAI_ENDPOINT if AZURE_OPENAI_ENDPOINT else f"https://{AZURE_OPENAI_RESOURCE}.openai.azure.com/"
     endpoint = f"{base_url}openai/deployments/{AZURE_OPENAI_MODEL}/extensions/chat/completions?api-version={AZURE_OPENAI_PREVIEW_API_VERSION}"
     history_metadata = request_body.get("history_metadata", {})
-
     if not SHOULD_STREAM:
         r = requests.post(endpoint, headers=headers, json=body)
         status_code = r.status_code
         r = r.json()
         r['history_metadata'] = history_metadata
-
         return Response(format_as_ndjson(r), status=status_code)
     else:
         return Response(stream_with_data(body, headers, endpoint, history_metadata), mimetype='text/event-stream')
-
-
 def stream_without_data(response, history_metadata={}):
     responseText = ""
     for line in response:
         deltaText = line["choices"][0]["delta"].get('content')
         if deltaText and deltaText != "[DONE]":
             responseText += deltaText
-
         response_obj = {
             "id": line["id"],
             "model": line["model"],
@@ -284,14 +242,11 @@ def stream_without_data(response, history_metadata={}):
             "history_metadata": history_metadata
         }
         yield format_as_ndjson(response_obj)
-
-
 def conversation_without_data(request_body):
     openai.api_type = "azure"
     openai.api_base = AZURE_OPENAI_ENDPOINT if AZURE_OPENAI_ENDPOINT else f"https://{AZURE_OPENAI_RESOURCE}.openai.azure.com/"
     openai.api_version = "2023-03-15-preview"
     openai.api_key = AZURE_OPENAI_KEY
-
     request_messages = request_body["messages"]
     messages = [
         {
@@ -299,13 +254,11 @@ def conversation_without_data(request_body):
             "content": AZURE_OPENAI_SYSTEM_MESSAGE
         }
     ]
-
     for message in request_messages:
         messages.append({
             "role": message["role"] ,
             "content": message["content"]
         })
-
     response = openai.ChatCompletion.create(
         engine=AZURE_OPENAI_MODEL,
         messages = messages,
@@ -315,9 +268,7 @@ def conversation_without_data(request_body):
         stop=AZURE_OPENAI_STOP_SEQUENCE.split("|") if AZURE_OPENAI_STOP_SEQUENCE else None,
         stream=SHOULD_STREAM
     )
-
     history_metadata = request_body.get("history_metadata", {})
-
     if not SHOULD_STREAM:
         response_obj = {
             "id": response,
@@ -332,17 +283,13 @@ def conversation_without_data(request_body):
             }],
             "history_metadata": history_metadata
         }
-
         return jsonify(response_obj), 200
     else:
         return Response(stream_without_data(response, history_metadata), mimetype='text/event-stream')
-
-
 @app.route("/conversation", methods=["GET", "POST"])
 def conversation():
     request_body = request.json
     return conversation_internal(request_body)
-
 def conversation_internal(request_body):
     try:
         use_data = should_use_data()
@@ -353,21 +300,17 @@ def conversation_internal(request_body):
     except Exception as e:
         logging.exception("Exception in /conversation")
         return jsonify({"error": str(e)}), 500
-
 ## Conversation History API ## 
 @app.route("/history/generate", methods=["POST"])
 def add_conversation():
     authenticated_user = get_authenticated_user_details(request_headers=request.headers)
     user_id = authenticated_user['user_principal_id']
-
     ## check request for conversation_id
     conversation_id = request.json.get("conversation_id", None)
-
     try:
         # make sure cosmos is configured
         if not cosmos_conversation_client:
             raise Exception("CosmosDB is not configured")
-
         # check for the conversation_id, if the conversation is not set, we will create a new one
         history_metadata = {}
         if not conversation_id:
@@ -398,21 +341,16 @@ def add_conversation():
     except Exception as e:
         logging.exception("Exception in /history/generate")
         return jsonify({"error": str(e)}), 500
-
-
 @app.route("/history/update", methods=["POST"])
 def update_conversation():
     authenticated_user = get_authenticated_user_details(request_headers=request.headers)
     user_id = authenticated_user['user_principal_id']
-
     ## check request for conversation_id
     conversation_id = request.json.get("conversation_id", None)
-
     try:
         # make sure cosmos is configured
         if not cosmos_conversation_client:
             raise Exception("CosmosDB is not configured")
-
         # check for the conversation_id, if the conversation is not set, we will create a new one
         if not conversation_id:
             raise Exception("No conversation_id found")
@@ -444,7 +382,6 @@ def update_conversation():
     except Exception as e:
         logging.exception("Exception in /history/update")
         return jsonify({"error": str(e)}), 500
-
 @app.route("/history/delete", methods=["DELETE"])
 def delete_conversation():
     ## get the user id from the request headers
@@ -459,40 +396,31 @@ def delete_conversation():
         
         ## delete the conversation messages from cosmos first
         deleted_messages = cosmos_conversation_client.delete_messages(conversation_id, user_id)
-
         ## Now delete the conversation 
         deleted_conversation = cosmos_conversation_client.delete_conversation(user_id, conversation_id)
-
         return jsonify({"message": "Successfully deleted conversation and messages", "conversation_id": conversation_id}), 200
     except Exception as e:
         logging.exception("Exception in /history/delete")
         return jsonify({"error": str(e)}), 500
-
 @app.route("/history/list", methods=["GET"])
 def list_conversations():
     authenticated_user = get_authenticated_user_details(request_headers=request.headers)
     user_id = authenticated_user['user_principal_id']
-
     ## get the conversations from cosmos
     conversations = cosmos_conversation_client.get_conversations(user_id)
     if not isinstance(conversations, list):
         return jsonify({"error": f"No conversations for {user_id} were found"}), 404
-
     ## return the conversation ids
-
     return jsonify(conversations), 200
-
 @app.route("/history/read", methods=["POST"])
 def get_conversation():
     authenticated_user = get_authenticated_user_details(request_headers=request.headers)
     user_id = authenticated_user['user_principal_id']
-
     ## check request for conversation_id
     conversation_id = request.json.get("conversation_id", None)
     
     if not conversation_id:
         return jsonify({"error": "conversation_id is required"}), 400
-
     ## get the conversation object and the related messages from cosmos
     conversation = cosmos_conversation_client.get_conversation(user_id, conversation_id)
     ## return the conversation id and the messages in the bot frontend format
@@ -501,17 +429,13 @@ def get_conversation():
     
     # get the messages for the conversation from cosmos
     conversation_messages = cosmos_conversation_client.get_messages(user_id, conversation_id)
-
     ## format the messages in the bot frontend format
     messages = [{'id': msg['id'], 'role': msg['role'], 'content': msg['content'], 'createdAt': msg['createdAt']} for msg in conversation_messages]
-
     return jsonify({"conversation_id": conversation_id, "messages": messages}), 200
-
 @app.route("/history/rename", methods=["POST"])
 def rename_conversation():
     authenticated_user = get_authenticated_user_details(request_headers=request.headers)
     user_id = authenticated_user['user_principal_id']
-
     ## check request for conversation_id
     conversation_id = request.json.get("conversation_id", None)
     
@@ -522,22 +446,18 @@ def rename_conversation():
     conversation = cosmos_conversation_client.get_conversation(user_id, conversation_id)
     if not conversation:
         return jsonify({"error": f"Conversation {conversation_id} was not found. It either does not exist or the logged in user does not have access to it."}), 404
-
     ## update the title
     title = request.json.get("title", None)
     if not title:
         return jsonify({"error": "title is required"}), 400
     conversation['title'] = title
     updated_conversation = cosmos_conversation_client.upsert_conversation(conversation)
-
     return jsonify(updated_conversation), 200
-
 @app.route("/history/delete_all", methods=["DELETE"])
 def delete_all_conversations():
     ## get the user id from the request headers
     authenticated_user = get_authenticated_user_details(request_headers=request.headers)
     user_id = authenticated_user['user_principal_id']
-
     # get conversations for user
     try:
         conversations = cosmos_conversation_client.get_conversations(user_id)
@@ -548,17 +468,14 @@ def delete_all_conversations():
         for conversation in conversations:
             ## delete the conversation messages from cosmos first
             deleted_messages = cosmos_conversation_client.delete_messages(conversation['id'], user_id)
-
             ## Now delete the conversation 
             deleted_conversation = cosmos_conversation_client.delete_conversation(user_id, conversation['id'])
-
         return jsonify({"message": f"Successfully deleted conversation and messages for user {user_id}"}), 200
     
     except Exception as e:
         logging.exception("Exception in /history/delete_all")
         return jsonify({"error": str(e)}), 500
     
-
 @app.route("/history/clear", methods=["POST"])
 def clear_messages():
     ## get the user id from the request headers
@@ -573,12 +490,10 @@ def clear_messages():
         
         ## delete the conversation messages from cosmos
         deleted_messages = cosmos_conversation_client.delete_messages(conversation_id, user_id)
-
         return jsonify({"message": "Successfully deleted messages in conversation", "conversation_id": conversation_id}), 200
     except Exception as e:
         logging.exception("Exception in /history/clear_messages")
         return jsonify({"error": str(e)}), 500
-
 @app.route("/history/ensure", methods=["GET"])
 def ensure_cosmos():
     if not AZURE_COSMOSDB_ACCOUNT:
@@ -586,17 +501,12 @@ def ensure_cosmos():
     
     if not cosmos_conversation_client or not cosmos_conversation_client.ensure():
         return jsonify({"error": "CosmosDB is not working"}), 500
-
     return jsonify({"message": "CosmosDB is configured and working"}), 200
-
-
 def generate_title(conversation_messages):
     ## make sure the messages are sorted by _ts descending
     title_prompt = 'Summarize the conversation so far into a 4-word or less title. Do not use any quotation marks or punctuation. Respond with a json object in the format {{"title": string}}. Do not include any other commentary or description.'
-
     messages = [{'role': msg['role'], 'content': msg['content']} for msg in conversation_messages]
     messages.append({'role': 'user', 'content': title_prompt})
-
     try:
         ## Submit prompt to Chat Completions for response
         base_url = AZURE_OPENAI_ENDPOINT if AZURE_OPENAI_ENDPOINT else f"https://{AZURE_OPENAI_RESOURCE}.openai.azure.com/"
@@ -614,6 +524,5 @@ def generate_title(conversation_messages):
         return title
     except Exception as e:
         return messages[-2]['content']
-
 if __name__ == "__main__":
     app.run()
